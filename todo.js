070e176e37748f67ff59c439c08e6046c69ac7be
jacy <johnsoncr.hong@gmail.com>
2017-08-09 12:19:27 +0800

    use ref to control todo text

--- todo.js
+++ todo.js
@@ -1,11 +1,7 @@
 class App extends React.Component{
   constructor(state){
     super(state);
-    this.state = {toDos:[], filter:'All', newTodoText: ''};
-  }
-  
-  onAddToDoTextChange(text){
-    this.setState({newTodoText:text});
+    this.state = {toDos:[], filter:'All'};
   }
   
   onFilterChange(filter){
@@ -19,8 +15,10 @@ class App extends React.Component{
   }
   
   onAddToDo(){
+    let newTodo = this.newAddTodo.value;
+    this.newAddTodo.value = '';
     this.setState((preState,props) => {
-      return {toDos: preState.toDos.concat({text: preState.newTodoText, completed: false, id: preState.toDos.length + 1}), newTodoText : ''}
+      return {toDos: preState.toDos.concat({text: newTodo, completed: false, id: preState.toDos.length + 1})}
     });
   }
   
@@ -32,7 +30,7 @@ class App extends React.Component{
     let filter= this.state.filter;
     let newToDos= this.state.toDos.filter(i => filter == 'All' || i.completed == (filter == 'Complete'));
      return <div>
-       <AddToDo text={this.state.newTodoText} onAddToDoTextChange={text => this.onAddToDoTextChange(text)} onAddToDo={e => this.onAddToDo()}/>
+       <AddToDo inputRef={input => this.newAddTodo = input} onAddToDo={() => this.onAddToDo()}/>
        <ShowToDos toDos={newToDos} onClick={id => this.onToggleToDo(id)}/>
        <Footer onFilterChange={filter => this.onFilterChange(filter)} filters={['All', 'Active', 'Complete']} selectedFilter={filter} />
      </div>
@@ -41,23 +39,23 @@ class App extends React.Component{
 }
        
 function AddToDo(props){
-      return <div><input value={props.text} onChange={e => props.onAddToDoTextChange(e.target.value)} />
-        <button onClick={e => props.onAddToDo()}>Add Todo </button> </div>
+      return <div><input ref={props.inputRef}/>
+        <button onClick={props.onAddToDo}>Add Todo </button> </div>
 }
        
 function ShowToDos(props){
-      return <ul>{props.toDos.map(item => <ToDo text={item.text} key={item.id} id={item.id} className={item.completed? 'completed': 'incompleted'} onClick={id => props.onClick(id)}/>)}</ul>
+      return <ul>{props.toDos.map(item => <ToDo text={item.text} key={item.id} className={item.completed? 'completed': 'incompleted'} onClick={() => props.onClick(item.id)}/>)}</ul>
 }
        
 function ToDo(props){
-      return <li id={props.id} onClick={e => props.onClick(e.target.id)} className={props.className}>{props.text}</li>
+      return <li onClick={props.onClick} className={props.className}>{props.text}</li>
 }
 
 function Footer(props){
-      return <div>Show: { props.filters.map((item, i) => <Filter name={item} key={i} selected={item == props.selectedFilter} onFilterChange={filter => props.onFilterChange(filter)}/>) } </div>
+      return <div>Show: { props.filters.map((item, i) => <Filter name={item} key={i} selected={item == props.selectedFilter} onFilterChange={() => props.onFilterChange(item)}/>) } </div>
 }
 
 function Filter(props){
-	return props.selected ? <span> {props.name} </span> :  <span> <a href="#" onClick={e=> props.onFilterChange(props.name)}>{props.name}</a></span>
+	return <span> {props.selected ? props.name :  <a href="#" onClick={props.onFilterChange}>{props.name}</a>} </span>
 }
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
